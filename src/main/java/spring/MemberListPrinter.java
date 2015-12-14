88285be1cc504fa6f87c74a2a83d0c6d731d9b37
JHTA <JHTA@JHTA-PC>
2015-12-14 15:40:18 +0900

    m1_annotation 테스트목적 커밋

---
+++ src/main/java/spring/MemberListPrinter.java
@@ -0,0 +1,20 @@
+package spring;
+
+import java.util.Collection;
+
+public class MemberListPrinter {
+	private MemberDao memberdao;
+	private MemberPrinter printer;
+	
+	public MemberListPrinter(MemberDao memberDao, MemberPrinter printer){
+		this.memberdao = memberDao;
+		this.printer = printer;
+	}
+	
+	public void selectAll(){
+		Collection<Member> members = memberdao.selectAll();
+		for(Member m : members){
+			printer.print(m);
+		}
+	}
+}
---
+++ src/main/java/spring/MemberPrinter.java
@@ -0,0 +1,10 @@
+package spring;
+
+
+//전체 회원을 조회해서 화면에 출력해주는 서비스 담당
+public class MemberPrinter {
+	public void print(Member member){
+		System.out.printf("회원정보 : 아이디=%d, 이메일=%s, 등록일=%tF\n",
+				member.getId(), member.getEmail(), member.getRegisterDate());
+	}
+}
